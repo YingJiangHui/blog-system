182ebcf548ea4fcf5084ca48065df7de870f8397
dark <473380917@qq.com>
2020-12-10 22:36:22 +0800

    nextjs+typeORM
    init
    
    typeorm init
    
    typeorm install
    
    babel 未配置完成
    
    reslove babel question
    
    取消数据库字段名自动同步功能
    
    migration table
    
    nextjs+typeORM基础å¼M开发脚手架
    
    nextjs+typeORM基础å¼M开发脚手架

---
+++ src/migration/1607954596677-CreatePosts.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreatePosts1607954596677 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
