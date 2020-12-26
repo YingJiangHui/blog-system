89fccf20f9261dc68bcdc2ba16be2c6a0998237f
dark <473380917@qq.com>
2020-12-26 12:46:14 +0800

    设置.env.local环境变量

--- lib/whitSession.ts
+++ lib/whitSession.ts
@@ -1,13 +1,9 @@
-// @ts-ignore
 import { withIronSession } from 'next-iron-session'
 import {NextApiHandler} from "next";
-
 export default function withSession(handler:NextApiHandler) {
   return withIronSession(handler, {
-    password: "4d5057cc-653f-4e18-b1f4-11dc0ff775bb",
+    password: process.env.SECRET,
     cookieName: 'blog',
-    // password: process.env.SECRET_COOKIE_PASSWORD,
-    // cookieName: 'next.js/examples/with-iron-session',
     cookieOptions:{
       secure:false
     }
