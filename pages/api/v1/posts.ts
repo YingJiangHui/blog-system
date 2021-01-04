dcf8f9d004ad25474c97e659650818696947ade4
Ying <473380917@qq.com>
2021-01-04 17:02:00 +0800

    添加编辑post功能

--- pages/api/v1/posts.ts
+++ pages/api/v1/posts.ts
@@ -4,15 +4,11 @@ import {Post} from '../../../src/entity/Post';
 import {getDatabaseConnection} from '../../../lib/getDatabaseConnection';
 import withSession from '../../../lib/whitSession';
 import {User} from '../../../src/entity/User';
+import {EntityManager} from 'typeorm/index';
 
 export default withSession(async(req: NextApiRequest,res: NextApiResponse) => {
-  const methodMap = {
-    'GET': async() => {
-      const posts = await getPosts();
-      res.statusCode = 200;
-      res.setHeader('Content-type','application/json');
-      res.json(posts);
-    },'POST': async() => {
+  const updatePosts = ():Promise<{manager:EntityManager,posts:Post}>=>{
+    return new Promise(async(resolve, reject)=>{
       const {manager} = await getDatabaseConnection();
       const {title,content} = req.body;
       const posts = new Post();
@@ -20,9 +16,21 @@ export default withSession(async(req: NextApiRequest,res: NextApiResponse) => {
       posts.content = content;
       posts.validate()
       if(posts.hasError()){
-        res.statusCode = 422
-        res.json(posts.errors)
-      }else{
+        reject(posts.errors)
+      }else {
+        resolve({manager,posts})
+      }
+    })
+  }
+  
+  const methodMap = {
+    'GET': async() => {
+      const posts = await getPosts();
+      res.statusCode = 200;
+      res.setHeader('Content-type','application/json');
+      res.json(posts);
+    },'POST': async() => {
+      updatePosts().then(async ({manager,posts})=>{
         const username = req.session.get('currentUser');
         posts.author = await manager.findOne(User,{
           where: {
@@ -30,16 +38,31 @@ export default withSession(async(req: NextApiRequest,res: NextApiResponse) => {
           }
         });
         await manager.save(posts);
-        res.statusCode = 200;
-        res.json(posts);
-      }
+        res.json({posts:posts})
+      },(errors)=>{
+        res.statusCode = 422
+        res.json(errors)
+      })
     },'DELETE': async() => {
       const {manager} = await getDatabaseConnection();
       const body = req.body;
       res.statusCode = 200;
       await manager.delete(Post,body.id);
       res.json({postsId:body.id});
+    },'PATCH':async()=>{
+      updatePosts().then(async ({manager,posts})=>{
+        const body = req.body;
+        res.statusCode = 200
+        await manager.createQueryBuilder().update(Post)
+        .set({ title: posts.title, content: posts.content })
+        .where("id = :id", { id: body.id })
+        .execute();
+        res.json({posts:posts});
+      },(errors)=>{
+        res.statusCode = 422
+        res.json(errors)
+      })
     }
   };
-  await methodMap[req.method as 'GET'|'POST'|'DELETE']();
+  await methodMap[req.method as 'GET'|'POST'|'DELETE'|'PATCH']();
 });
