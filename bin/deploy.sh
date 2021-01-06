5bbd3fc6fd478687492a393ae5fb483c7fb01f59
Ying <473380917@qq.com>
2021-01-06 18:04:03 +0800

    deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,9 @@
 echo 'start';
-docker start f5b1 &&
+docker start 7b5a &&
+docker exec -it 7b5a bash &&
+psql -U blog &&
+CREATE DATABASE blog_development ENCODING 'UTF8' LC_COLLATE 'en_US.utf8' LC_CTYPE 'en_US.utf8' &&
+
 cd /home/blog/app/ &&
 git pull &&
 yarn install --production=false &&
@@ -8,8 +12,8 @@ git apply migrate.patch;
 yarn compile &&
 yarn migration:run &&
 git reset --hard HEAD &&
-docker build -t fang/node-web-app . &&
+docker build -t ying/node-web-app . &&
 docker kill app &&
 docker rm app &&
-docker run --name app --network=host -p 3000:3000 -d fang/node-web-app &&
+docker run --name app --network=host -p 3000:3000 -d ying/node-web-app &&
 echo 'OK!'
