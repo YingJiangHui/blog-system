3e8c39ec05c4f8a1c49bb250382d789831c6493c
dark <473380917@qq.com>
2021-01-06 21:32:46 +0800

    update

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,9 +1,5 @@
 echo 'start';
 docker start 7b5a &&
-docker exec -it 7b5a bash &&
-psql -U blog &&
-CREATE DATABASE blog_development ENCODING 'UTF8' LC_COLLATE 'en_US.utf8' LC_CTYPE 'en_US.utf8' &&
-
 cd /home/blog/app/ &&
 git pull &&
 yarn install --production=false &&
